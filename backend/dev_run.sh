c95749679c84b79f914448c3f21693b89025f9a7
Jamie <jamieha9@gmail.com>
2025-12-08 13:00:33 +1100

    chore: Updated dev_run script, settings.py so that both the backend and postgresql db share the same .env file

--- backend/dev_run.sh
+++ backend/dev_run.sh
@@ -5,11 +5,11 @@ IMAGE_NAME="local-postgres"
 # Check if the image exists
 if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
     echo "Docker image '$IMAGE_NAME' is present."
+    
+else
+    echo "Docker image '$IMAGE_NAME' is NOT present."
     echo "Running docker build..."
     docker build -t "$IMAGE_NAME" .
-else
-    echo "Docker image '$IMAGE_NAME' is NOT present. Skipping build."
 fi
 
-docker run -d -p 5432:5432 $IMAGE_NAME && python3 manage.py runserver
-
+docker run --env-file .env -p 5432:5432 $IMAGE_NAME && python3 manage.py runserver
