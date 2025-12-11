36e88faa727ec6245be00edf86b5739f2ca9c6aa
Jamie <jamieha9@gmail.com>
2025-12-12 00:48:42 +1100

    feat: Added new urlpatterns

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_id>', views.CommentUpdateView.as_view()),
+    path('<int:post_id>', views.CommentGetView.as_view()),
+    path('<int:comment_id>', views.CommentUpdateView.as_view()),
     path('reply/<int:comment_id>', views.ReplyCommentView.as_view()),
 ]
