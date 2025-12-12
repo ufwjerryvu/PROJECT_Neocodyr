cccfc99faa300dc1aaca7a2e1b8aed4f5f6b6db3
Jamie <jamieha9@gmail.com>
2025-12-13 01:17:50 +1100

    fix: changed pathname from verbs to nouns

--- backend/posting/urls.py
+++ backend/posting/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_id>', views.GetPostsView.as_view()),
-    path('forum/<int:course_id>', views.BatchPostsView.as_view()),
-    path('create', views.CreatePostView.as_view()),
-    path('update/<int:post_id>', views.UpdatePostsView.as_view()),
-    
+    path('batch/<int:course_id>', views.BatchPostsView.as_view()), # GET 
+    path('single/<int:post_id>', views.GetPostsView.as_view()), # GET
+    path('post/', views.CreatePostView.as_view()), # POST
+    path('post/<int:post_id>', views.UpdatePostsView.as_view()), # PATCH 
 ]
