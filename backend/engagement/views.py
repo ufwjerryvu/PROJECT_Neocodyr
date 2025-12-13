58b88e7cf00b5d53c22d31f44aa7fc734165b0df
Jamie <jamieha9@gmail.com>
2025-12-13 15:54:09 +1100

    feat: added delete into view

--- backend/engagement/views.py
+++ backend/engagement/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework import permissions
+
+from .models import LikesPost, LikesComment
 from .serializer import PostLikeSerializer, CommentLikeSerializer
 
 class ReactsPostView(APIView):
@@ -20,7 +22,12 @@ class ReactsPostView(APIView):
         return Response({"error": "Failed to like the post"}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, post_id):
-        return Response({"error": "Failed to remove like from the post"}, status=status.HTTP_400_BAD_REQUEST)
+        delete_result = LikesComment.objects.filter(
+            user = request.user.id,
+            comment=post_id
+        ).delete()
+        
+        return Response({"message": f"Amount of rows removed {delete_result[0]}"}, status=status.HTTP_204_NO_CONTENT)
     
 class ReactsCommentView(APIView):
     """
@@ -37,4 +44,9 @@ class ReactsCommentView(APIView):
         return Response({"error": "Failed to like the comment"}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, comment_id):
-        return Response({"error": "Failed to remove like from the comment"}, status=status.HTTP_400_BAD_REQUEST)
+        delete_result = LikesComment.objects.filter(
+            user = request.user.id,
+            comment=comment_id
+        ).delete()
+        
+        return Response({"message": f"Amount of rows removed {delete_result[0]}"}, status=status.HTTP_204_NO_CONTENT)
