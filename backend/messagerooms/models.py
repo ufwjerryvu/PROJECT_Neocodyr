bb9bd4a4c19908e076a54fd95b73058827cd44ca
Jamie <jamieha9@gmail.com>
2025-12-15 15:56:46 +1100

    feat: Added the room and keys for messages

--- backend/messagerooms/models.py
+++ backend/messagerooms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from rest_framework import serializers
+from users.models import User
 
-# Create your models here.
+class Room(serializers.ModelSerializer):
+    name = models.CharField(max_length=256)
+    room_recieve_key = models.BinaryField()
+    created_at = models.DateTimeField(nullable=False, blank=False)
+    type = models.CharField(max_length=256)
+    user = models.ForeignKey(User, nullable=False, blank=False)
+         
+class Keys(serializers.ModelSerializer): 
+    room_recieve_key = models.BinaryField()
+    created_at = models.DateTimeField(nullable=False, blank=False)
+    updated_at = models.DateTimeField()
+    user = models.ForeignKey(User, nullable=False, blank=False)
+    room = models.ForeignKey(Room, nullable=False, blank=False)
