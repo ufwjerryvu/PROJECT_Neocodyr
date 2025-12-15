7a096078688ab61b37f268119f9d35f6411f4b80
Jamie <jamieha9@gmail.com>
2025-12-15 14:16:28 +1100

    fix: Added try and except on delete functions for likes on post and comment

--- backend/engagement/views.py
+++ backend/engagement/views.py
@@ -17,18 +17,21 @@ class ReactsPostView(APIView):
         serializer_class = PostLikeSerializer(data=request.data)
         if serializer_class.is_valid():
             serializer_class.save()
-            return Response({"message": f"successfully liked post"}, status=status.HTTP_201_CREATED)
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
 
-        return Response({"error": "Failed to like the post"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, post_id):
-        delete_result = LikesPost.objects.filter(
-            user = request.user.id,
-            comment=post_id
-        ).delete()
-        
-        return Response({"message": f"Amount of rows removed {delete_result[0]}"}, status=status.HTTP_204_NO_CONTENT)
-    
+        try:
+            LikesPost.objects.filter(
+                user = request.user.id,
+                comment=post_id
+            ).delete()
+            
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 class ReactsCommentView(APIView):
     """
     For the purpose of add and removing the like / reaction to the comment. 
@@ -39,14 +42,18 @@ class ReactsCommentView(APIView):
         serializer_class = CommentLikeSerializer(data=request.data)
         if serializer_class.is_valid():
             serializer_class.save()
-            return Response({"message": f"successfully liked comment"}, status=status.HTTP_201_CREATED)
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
 
-        return Response({"error": "Failed to like the comment"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, comment_id):
-        delete_result = LikesComment.objects.filter(
-            user = request.user.id,
-            comment=comment_id
-        ).delete()
+        try:
+            LikesComment.objects.filter(
+                user = request.user.id,
+                comment=comment_id
+            ).delete()
+            
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         
-        return Response({"message": f"Amount of rows removed {delete_result[0]}"}, status=status.HTTP_204_NO_CONTENT)
